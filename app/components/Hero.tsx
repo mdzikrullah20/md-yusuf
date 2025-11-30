3dffd70cb571aed0402afe2e5b37cbf7247ab347
Md Zikrullah <work.zikrullah@gmail.com>
2025-12-01 01:51:50 +0530

    fixed image mobile size

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -213,12 +213,12 @@ export default function Hero() {
           }}
           className="relative lg:w-1/2 flex justify-center items-center order-1 lg:order-2"
         >
-          <div className=" relative w-84 h-106 sm:w-64 sm:h-64 md:w-100 md:h-100 lg:w-100 lg:h-[600px] rounded-md overflow-hidden cursor-pointer">
+          <div className="border-2 relative w-84 h-120 sm:w-64 sm:h-64 md:w-100 md:h-100 lg:w-100 lg:h-[600px] rounded-md overflow-hidden cursor-pointer">
             <Image
               src="/Images/raza.png"
               alt="Zikrullah's Profile Picture"
               fill
-              className="object-cover grayscale hover:grayscale-0 transition-all duration-500 rounded-2xl"
+              className="object-cover grayscale hover:grayscale-0 transition-all duration-500"
             />
           </div>
         </motion.div>
